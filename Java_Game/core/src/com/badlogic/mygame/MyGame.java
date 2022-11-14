687ddcc08ff076a79f6ad0306c47ae5a6ade0fb6
Saumil <saumil21097@iiitd.ac.in>
2022-11-14 21:10:50 +0530

    need to make HUD(Video 4)

--- Java_Game/core/src/com/badlogic/mygame/MyGame.java
+++ Java_Game/core/src/com/badlogic/mygame/MyGame.java
@@ -1,32 +1,36 @@
 package com.badlogic.mygame;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class MyGame extends ApplicationAdapter {
-	SpriteBatch batch;
+public class MyGame extends Game {
+	public SpriteBatch batch;
 	Texture img;
+	BitmapFont Font;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		Font = new BitmapFont();
+		setScreen(new PlayScreen(this));
 	}
 
 	@Override
 	public void render () {
-		//kaise ho????
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+		Font.dispose();
 	}
 }
--- Java_Game/core/src/com/badlogic/mygame/PlayScreen.java
+++ Java_Game/core/src/com/badlogic/mygame/PlayScreen.java
@@ -1,2 +1,65 @@
-package com.badlogic.mygame;public class PlayScreen {
+package com.badlogic.mygame;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+public class PlayScreen implements Screen {
+    private MyGame game;
+    Texture texture;
+    private OrthographicCamera game_camera;
+    private Viewport game_port;
+    public PlayScreen(MyGame game){
+        this.game = game;
+        texture = new Texture("badlogic.jpg");
+        game_camera=new OrthographicCamera();
+//        game_port=new ScreenViewport(game_camera);                      //different configs for game screen
+//      game_port=new StretchViewport(800,400,game_camera);                 //different configs for game screen
+        game_port=new FitViewport(800,480,game_camera);  //different configs for game screen
+    }
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.setProjectionMatrix(game_camera.combined);
+        game.batch.begin();
+        game.batch.draw(texture,0,0);
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        game_port.update(width,height);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
