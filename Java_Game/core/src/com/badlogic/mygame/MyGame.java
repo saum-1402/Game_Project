b626681d23d36c9e62a29e838a492a34a27f5741
Saumil <saumil21097@iiitd.ac.in>
2022-11-15 01:07:41 +0530

    saumil

--- Java_Game/core/src/com/badlogic/mygame/MyGame.java
+++ Java_Game/core/src/com/badlogic/mygame/MyGame.java
@@ -10,6 +10,9 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGame extends Game {
+	public static final int V_WIDTH=400;
+	public static final int V_HEIGHT=400;
+
 	public SpriteBatch batch;
 	Texture img;
 	BitmapFont Font;
