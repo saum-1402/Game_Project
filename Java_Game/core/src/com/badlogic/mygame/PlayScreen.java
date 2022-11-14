3a4c9072489741ff8f745f9ac8db794a09dd08d3
Sankar <sanskar21096@iiitd.ac.in>
2022-11-15 01:07:33 +0530

    messagewa

--- Java_Game/core/src/com/badlogic/mygame/PlayScreen.java
+++ Java_Game/core/src/com/badlogic/mygame/PlayScreen.java
@@ -9,7 +9,8 @@ import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
-//asdbhasbdj h`
+
+//sanskar ka comment
 public class PlayScreen implements Screen {
     private MyGame game;
     Texture texture;
