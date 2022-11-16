1bd4591d4902aafb4163ae0fdb7cae24f7baf7de
Saumil <saumil21097@iiitd.ac.in>
2022-11-17 00:46:01 +0530

    added HUD

--- Java_Game/core/src/com/badlogic/mygame/MyGame.java
+++ Java_Game/core/src/com/badlogic/mygame/MyGame.java
@@ -11,7 +11,7 @@ import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGame extends Game {
 	public static final int V_WIDTH=400;
-	public static final int V_HEIGHT=400;
+	public static final int V_HEIGHT=208;
 
 	public SpriteBatch batch;
 	Texture img;
--- Java_Game/core/src/com/badlogic/mygame/PlayScreen.java
+++ Java_Game/core/src/com/badlogic/mygame/PlayScreen.java
@@ -1,5 +1,6 @@
 package com.badlogic.mygame;
 
+import Scenes.HUD;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
@@ -10,19 +11,19 @@ import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
-//sanskar ka comment
+
 public class PlayScreen implements Screen {
     private MyGame game;
-    Texture texture;
+
     private OrthographicCamera game_camera;
     private Viewport game_port;
     public PlayScreen(MyGame game){
         this.game = game;
-        texture = new Texture("badlogic.jpg");
         game_camera=new OrthographicCamera();
-//        game_port=new ScreenViewport(game_camera);                      //different configs for game screen
+//      game_port=new ScreenViewport(game_camera);                      //different configs for game screen
 //      game_port=new StretchViewport(800,400,game_camera);                 //different configs for game screen
-        game_port=new FitViewport(800,480,game_camera);  //different configs for game screen
+        game_port=new FitViewport(MyGame.V_WIDTH,MyGame.V_HEIGHT,game_camera);  //different configs for game screen
+        HUD = new HUD(game.batch);
     }
     @Override
     public void show() {
@@ -33,10 +34,9 @@ public class PlayScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        game.batch.setProjectionMatrix(game_camera.combined);
-        game.batch.begin();
-        game.batch.draw(texture,0,0);
-        game.batch.end();
+
+        game.batch.setProjectionMatrix(HUD.stage.getCamera().combined);
+
     }
 
     @Override
