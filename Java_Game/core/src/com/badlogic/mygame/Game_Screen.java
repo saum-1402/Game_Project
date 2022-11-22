9e5d5f6c3aae923029424c5ecacb13823ac173b6
Sankar <sanskar21096@iiitd.ac.in>
2022-11-22 05:48:04 +0530

    ek loading page jaise banaya and aage dekhta

--- Java_Game/core/src/com/badlogic/mygame/Game_Screen.java
+++ Java_Game/core/src/com/badlogic/mygame/Game_Screen.java
@@ -18,7 +18,7 @@ import com.badlogic.gdx.Input.Keys;
 import javax.swing.plaf.TextUI;
 
 
-public class PlayScreen implements Screen {
+public class Game_Screen implements Screen {
     final MyGame game;
     private OrthographicCamera game_camera;
     private Viewport game_port;
@@ -28,12 +28,12 @@ public class PlayScreen implements Screen {
     private Texture surface;
     private Texture background_texture;
     SpriteBatch spriteBatch = new SpriteBatch();
-    public PlayScreen(MyGame game){
+    public Game_Screen(MyGame game){
         this.game = game;
         game_camera=new OrthographicCamera();
 
         //image assets
-        background_texture = new Texture(Gdx.files.internal("badlogic.jpg"));
+        background_texture = new Texture(Gdx.files.internal("background_texture.jpg"));
         tank_image=new Texture(Gdx.files.internal("tank_image.png"));           //tank ka texture
         //add surface
 
@@ -62,17 +62,13 @@ public class PlayScreen implements Screen {
         game.batch.begin();
 
         game.batch.draw(background_texture, 0,0, MyGame.V_WIDTH, MyGame.V_HEIGHT);
-//        game.batch.draw(tank_image, 120,10, 30, 30);            //texture draw kiya hai
+        game.batch.draw(tank_image, 120,10, 30, 30);            //texture draw kiya hai
 //        game.batch.draw(surface, 0,0, MyGame.V_WIDTH, MyGame.V_HEIGHT);   //surface hasn't been added
 
-        game.font.draw(game.batch, "Welcome to Tank Wars!", MyGame.V_WIDTH/3, MyGame.V_HEIGHT/3);
-        game.font.draw(game.batch, "Click anywhere to begin!", MyGame.V_WIDTH/3, 2*MyGame.V_HEIGHT/3);
+//        game.font.draw(game.batch, "Welcome to Tank l;kWars!", MyGame.V_WIDTH/3, MyGame.V_HEIGHT/3);
+//        game.font.draw(game.batch, "Click anywhere to begin!", MyGame.V_WIDTH/3, 2*MyGame.V_HEIGHT/3);
         game.batch.end();
 
-        if (Gdx.input.isTouched()) {
-            game.setScreen(new Game_Screen(game));
-            dispose();
-        }
     }
 
     @Override
