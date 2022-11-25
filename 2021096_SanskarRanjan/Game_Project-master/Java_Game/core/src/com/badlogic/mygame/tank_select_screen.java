ec5b40e797a18b3cfe9099ce9a706e4c067e239e
Sankar <sanskar21096@iiitd.ac.in>
2022-11-26 00:02:23 +0530

    final1

---
+++ 2021096_SanskarRanjan/Game_Project-master/Java_Game/core/src/com/badlogic/mygame/tank_select_screen.java
@@ -0,0 +1,125 @@
+package com.badlogic.mygame;
+
+import Scenes.HUD;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+
+public class tank_select_screen implements Screen {
+    final MyGame game;
+    private OrthographicCamera game_camera;
+    private Viewport game_port;
+    private HUD hud;
+    public Stage stage;
+    private Texture background_texture;
+    SpriteBatch spriteBatch = new SpriteBatch();
+    public tank_select_screen(MyGame game){
+        this.game = game;
+        game_camera=new OrthographicCamera();
+
+        //image assets
+        background_texture = new Texture(Gdx.files.internal("Tank_select.jpg"));
+//        tank_image=new Texture(Gdx.files.internal("tank_image.png"));           //tank ka texture
+        //add surface
+
+        //hud
+        hud = new HUD(game.batch);
+
+        //add music
+
+        //for views
+        // game_port=new ScreenViewport(game_camera);                      //different configs for game screen
+        // game_port=new StretchViewport(800,400,game_camera);                 //different configs for game screen
+        game_port=new FitViewport(MyGame.V_WIDTH,MyGame.V_HEIGHT,game_camera);  //different configs for game screen
+
+
+    }
+    @Override
+    public void show() {
+        stage = new Stage(new ScreenViewport());
+        int Help_Guides = 12;
+        Skin mySkin = new Skin(Gdx.files.internal("skin/glassy-ui.json"));
+        int row_height = Gdx.graphics.getWidth() / 12;
+        int col_width = Gdx.graphics.getWidth() / 12;
+        Button button2 = new TextButton("Select panzer", mySkin, "small");
+        button2.setSize(col_width * 3, row_height);
+        button2.setPosition(col_width * 9/2, Gdx.graphics.getHeight() - row_height * 7);
+        button2.addListener(new InputListener() {
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+//                System.out.println("i m up");
+//                game.setScreen(new PlayScreen(game));
+                game.setScreen(new map_select_2(game));
+//               outputLabel.setText("Press a Button");
+                return;
+            }
+
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+//                outputLabel.setText("Pressed Text Button");
+//                game.setScreen(new PlayScreen(game));
+                game.setScreen(new map_select_2(game));
+                return true;
+
+            }
+        });
+        stage.addActor(button2);
+
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(0, 0, 0.2f, 1);
+        game_camera.update();
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+
+        game.batch.begin();
+
+        game.batch.draw(background_texture, 0,0, MyGame.V_WIDTH, MyGame.V_HEIGHT);
+        game.batch.end();
+        stage.act();
+        stage.draw();
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        game_port.update(width,height);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+//        background_texture.dispose();
+//        surface.dispose();
+//        tank_image.dispose();
+
+    }
+}
---
+++ 2021096_SanskarRanjan/Game_Project-master/Java_Game/desktop/src/com/badlogic/mygame/DesktopLauncher.java
@@ -0,0 +1,17 @@
+package com.badlogic.mygame;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.mygame.MyGame;
+
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setWindowedMode(800, 480);
+		config.setForegroundFPS(60);
+		config.useVsync(true);
+		config.setTitle("my-game");
+		new Lwjgl3Application(new MyGame(), config);
+	}
+}
